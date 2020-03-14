e769de7e7a6e9ce8946a8bdbad574578414d9b20
cleytonRR <cleytonrodrigues69@live.com>
2020-03-14 16:16:44 -0300

    Added state for inputs

--- src/screen/signup/index.js
+++ src/screen/signup/index.js
@@ -1,24 +1,71 @@
-import React from 'react'
-import { View, Text, Button, TextInput, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import {TextInputMask} from 'react-native-masked-text'
 
 export default function Signup() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [cpf, setCpf] = useState('')
+    const [idade, setIdade] = useState('')
+
+    function handleData() {
+        if(name === '' || email === '' || password === '' || cpf ==='' || idade === '') {
+            alert('Preencha todos os campos')
+        }
+    }
+
     return (
         <>
             <View style={styles.boxImage}>
                 <Image source={require('../../img/logoSec.png')} />
             </View>
             <View style={styles.form}>
-                <TextInput style={styles.input} placeholderTextColor="#3C3CF0" placeholder='Nome' />
+                <TextInput 
+                style={styles.input} 
+                placeholderTextColor="#3C3CF0" 
+                placeholder='Nome' 
+                value={name}
+                onChangeText={name => setName(name)}
+                />
 
-                <TextInput style={styles.input} placeholderTextColor="#3C3CF0" placeholder='Email' />
+                <TextInput 
+                style={styles.input} 
+                placeholderTextColor="#3C3CF0" 
+                keyboardType='email-address'
+                placeholder='Email'
+                value={email}
+                onChangeText={email => setEmail(email)} 
+                />
 
-                <TextInput style={styles.input} placeholderTextColor="#3C3CF0" secureTextEntry={true} placeholder='Senha' />
+                <TextInput 
+                style={styles.input} 
+                placeholderTextColor="#3C3CF0" 
+                secureTextEntry={true} 
+                placeholder='Senha'
+                value={password}
+                onChangeText={password => setPassword(password)} 
+                />
+                
+                <TextInputMask 
+                style={styles.input}
+                placeholder='CPF'
+                placeholderTextColor="#3C3CF0"
+                type={'cpf'}
+                value={cpf}
+                onChangeText={cpf => setCpf(cpf)}
+                />
 
-                <TextInput style={styles.input} placeholderTextColor="#3C3CF0" placeholder='CPF' />
+                <TextInput 
+                style={styles.input} 
+                placeholderTextColor="#3C3CF0" 
+                placeholder='Idade'
+                keyboardType='numeric' 
+                value={idade}
+                onChangeText={idade => setIdade(idade)}
+                />
 
-                <TextInput style={styles.input} placeholderTextColor="#3C3CF0" placeholder='Idade' />
-
-                <TouchableOpacity style={styles.btn} onPress={()=>{}}>
+                <TouchableOpacity style={styles.btn} onPress={handleData}>
                     <Text style={styles.btnText}>Adicionar usuário</Text>
                 </TouchableOpacity>
             </View>
@@ -27,13 +74,13 @@ export default function Signup() {
 }
 
 const styles = StyleSheet.create({
-    
+
     boxImage: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
     },
-    
+
     form: {
         flex: 2,
         width: '90%'
@@ -45,13 +92,13 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginHorizontal: 5,
         marginBottom: 20
-    }, 
+    },
     btn: {
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#329AF8',
         height: 40,
-    }, 
+    },
     btnText: {
         fontSize: 20,
         fontWeight: "bold",
