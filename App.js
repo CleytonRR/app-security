5355ca2ab0f587ee9a03179213757380cd44caa3
cleytonRR <cleytonrodrigues69@live.com>
2020-03-17 23:24:38 -0300

    Created struct for login

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import Signup from './src/screen/signup/index'
 import Signin from './src/screen/singin/index'
+import Home from './src/screen/home/index'
 
 const Stack = createStackNavigator()
 
 export default function App() {
+  const [login, setLogin] = useState(1)
   return (
     <NavigationContainer>
       <Stack.Navigator 
-      initialRouteName='signin'
       screenOptions={{
         headerStyle: {
           backgroundColor: '#2b62d9' 
@@ -19,20 +20,33 @@ export default function App() {
         headerTintColor: '#fff'
       }}
       >
-        <Stack.Screen 
-        name='signin' 
-        component={Signin} 
-        options={{
-          title: 'Logar'
-        }}
-        />
-        <Stack.Screen 
-        name='signup' 
-        component={Signup}
-        options={{
-          title: 'Cadastrar novo usuário'
-        }} 
-        />
+        {login == null ? (
+          <>
+          <Stack.Screen 
+          name='signin' 
+          component={Signin} 
+          options={{
+            title: 'Logar'
+          }}
+          />
+          <Stack.Screen 
+          name='signup' 
+          component={Signup}
+          options={{
+            title: 'Cadastrar novo usuário'
+          }}
+          />
+          </>
+        ): (
+          <Stack.Screen
+          name='home'
+          component={Home}
+          options={{
+            title: 'Chamadas realizadas'
+          }}
+          />
+        ) 
+      }
       </Stack.Navigator>
     </NavigationContainer>
   );
